e6a7aa40c0277b2385dfb1a50ec5cda9c54b7223
Samuel <15628653+swibrow@users.noreply.github.com>
2024-04-27 22:27:23 +0200

    fix: fix cryptgeon chart

--- .devcontainer/tools/helm-docs.sh
+++ .devcontainer/tools/helm-docs.sh
@@ -8,4 +8,7 @@ os=$(uname -s)
 arch=$(uname -m)
 
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"$TOOL_VERSION"/helm-docs_"$TOOL_VERSION"_"$os"_"$arch".tar.gz
+tar -xf /tmp/helm-docs.tar.gz -C /usr/local/bin/ helm-docs
+rm /tmp/helm-docs.tar.gz
+
 helm-docs --version
